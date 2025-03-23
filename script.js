acbb4200dbf99499951458d984b2f21243eef239
Jhenryandoh <154546453+Jhenryandoh@users.noreply.github.com>
2025-03-23 06:58:24 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function shopNow() {
+    alert("Welcome to our shop! Browse our collection.");
+  }
+  
+  document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for contacting us! We'll get back to you soon.");
+  });
